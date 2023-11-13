4d4c652357bb30edaf476b5fdc4ad2ce4b2d6443
Dilshod Davletyarov <ddavlety@c4b7c6.42berlin.de>
2023-11-13 17:07:00 +0100

    2

--- main.c
+++ main.c
@@ -4,73 +4,99 @@
 #include <string.h>
 #include <ctype.h>
 
-
 int	main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("ISALPHA test:\n");
+	printf("___\nISALPHA test:\n");
 	printf("My solution:\n%i\n", ft_isalpha(*argv[1]));
 	printf("Standart library:\n%i\n\n", isalpha(*argv[1]));
 
-	printf("ISDIGIT test:\n");
+	printf("___\nISDIGIT test:\n");
 	printf("My solution:\n%i\n", ft_isdigit(*argv[1]));
 	printf("Standart library:\n%i\n\n", isdigit(*argv[1]));
 
-	printf("ISALNUM test:\n");
-	printf("My solution:\n%i\n", ft_isalnum(*argv[1]));
+	printf("___\nISALNUM test:\n");
+	printf("___\nMy solution:\n%i\n", ft_isalnum(*argv[1]));
 	printf("Standart library:\n%i\n\n", isalnum(*argv[1]));
 
-	printf("ISASCII test:\n");
-	printf("My solution:\n%i\n", ft_isascii(*argv[1]));
+	printf("___\nISASCII test:\n");
+	printf("___\nMy solution:\n%i\n", ft_isascii(*argv[1]));
 	printf("Standart library:\n%i\n\n", isascii(*argv[1]));
 
-	printf("ISPRINT test:\n");
+	printf("___\nISPRINT test:\n");
 	printf("My solution:\n%i\n", ft_isprint(*argv[1]));
 	printf("Standart library:\n%i\n\n", isprint(*argv[1]));
 
-	printf("STRLEN test:\n");
+	printf("___\nSTRLEN test:\n");
 	printf("My solution:\n%lu\n", ft_strlen(argv[1]));
 	printf("Standart library:\n%lu\n\n", strlen(argv[1]));
 
-
-	int	test[] = {1, 2, 3};
-	int	test1[] = {1, 2, 3};
-	int	i;
-	printf("MEMSET test:\n");
+	//MEMSET
+	char	test[] = "some string";
+	char	test1[] = "some string";
+	printf("___\nMEMSET test:\n");
 	printf("My solution:\n");
 	printf("Before change:\n");
-	i = 3;
-	while (i != 0)
-	{
-		printf("%i ", *test);
-		i--;
-	}
-	ft_memset(test, 42, 3);
+	printf("%s ", test);
+	ft_memset(test, '0', 3);
+	printf("\n");
 	printf("After change:\n");
-	i = 3;
-	while (i != 0)
-	{
-		printf("%i ", *test);
-		i--;
-	}
-	printf("Standart library:\n%lu\n\n", strlen(argv[1]));
-	i = 3;
-	while (i != 0)
-	{
-		printf("%i ", *test1);
-		i--;
-	}
-	memset(test1, 42, 3);
+	printf("%s ", test);
+	printf("\n\n");
+	printf("Standart library:\n");
+	printf("Before change:\n");
+	printf("%s ", test1);
+	memset(test1, 't', 3);
+	printf("\n");
 	printf("After change:\n");
-	i = 3;
-	while (i != 0)
-	{
-		printf("%i ", *test1);
-		i--;
-	}
+	printf("%s ", test1);
 	printf("\n\n");
 
-	printf("ATOI test:\n");
+	//BZERO
+	char	test2[] = "some string";
+	char	test3[] = "some string";
+	printf("___\nBZERO test2:\n");
+	printf("My solution:\n");
+	printf("Before change:\n");
+	printf("%s ", test2);
+	ft_bzero(test2, 2);
+	printf("\n");
+	printf("After change:\n");
+	printf("%s ", test2);
+	printf("\n\n");
+	printf("Standart library:\n");
+	printf("Before change:\n");
+	printf("%s ", test3);
+	bzero(test3, 2);
+	printf("\n");
+	printf("After change:\n");
+	printf("%s ", test3);
+	printf("\n\n");
+
+	//MEMCPY
+
+	char	test4[] = "some string";
+	char	test6[4];
+	char	test5[] = "some string";
+	char	test7[4];
+	printf("___\nMEMCPY test2:\n");
+	printf("My solution:\n");
+	printf("%s - ", test4);
+	ft_memcpy(test6, test4, 11);
+	printf("%s", test6);
+	printf("\n\n");
+	printf("Standart library:\n");
+	printf("%s - ", test5);
+	memcpy(test7, test5, 11);
+	printf("%s", test7);
+	printf("\n\n");
+
+
+
+
+	//ATOI
+
+	printf("___\nATOI test:\n");
 	printf("My solution:\n%i\n", ft_atoi(argv[1]));
 	printf("Standart library:\n%i\n\n", atoi(argv[1]));
 
