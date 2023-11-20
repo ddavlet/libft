f8febb665738c03c9294afabfc2bdc45cdf8f3b1
Dilshod Davletyarov <dilshod95@gmail.com>
2023-11-20 11:10:29 +0100

    __

--- 1/ft_strrchr.c
+++ 1/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ddavlety <ddavlety@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 12:27:32 by ddavlety          #+#    #+#             */
-/*   Updated: 2023/11/20 10:54:38 by ddavlety         ###   ########.fr       */
+/*   Updated: 2023/11/20 11:04:24 by ddavlety         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	unsigned int	i;
+	char			*ptr;
 
-	i = ft_strlen(s);
-	while (i >= 0)
+	i = 0;
+	ptr = NULL;
+	while (s[i])
 	{
-		if (s[i] = (char)c)
-			return (&s[i]);
-		i--;
+		if (s[i] == (char)c)
+			ptr = (char *)s;
+		i++;
 	}
-	return (NULL);
+	if (s[i] == (char)c)
+		ptr = (char *)s;
+	return (ptr);
 }
